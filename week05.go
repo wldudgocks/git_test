b82442bc94ddd374bdb846f9ea241b7dd03829a3
jiyeong <wldudgocks@naver.com>
2023-09-25 17:36:57 +0900

    v0.8

--- week05.go
+++ week05.go
@@ -5,14 +5,24 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
 	fmt.Print("Input score : ")
 	reader := bufio.NewReader(os.Stdin)
-	inputScore, err := reader.ReadString('\n') // option 2
+	inputScoreString, err := reader.ReadString('\n') // option 2
 	if err != nil {
 		log.Fatal(err)
 	}
+	inputScoreString = string.TrimSpace(inputScoreString) //remove space
+	inputScore, err := strconv.ParseFloat(inputScoreString, 32)
 	fmt.Println(inputScore)
+	if inputScore >= 90 {
+		grade := "A grade!"
+
+	}
+	else {
+		grade := "under A grade..."
+	}
 }
